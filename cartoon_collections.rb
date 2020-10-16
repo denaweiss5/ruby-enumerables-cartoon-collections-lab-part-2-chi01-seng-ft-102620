5073dad660abe1448d95c3a511d1b81f726f578d
Dena Neuman <denaweiss5@gmail.com>
2020-10-16 01:37:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,15 @@
 def square_array(array)
-  # Use an Enumerable to square every element in the passed in array
-  # Return a new array of the results
-end
+  new_array = []
+  index = 0
+  while index < array.length do
+    new_array << array[index] * array[index]
+    index +=1
+  end
+
+  new_array
+  end
+  square_array
+
 
 def summon_captain_planet(planeteer_calls)
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
@@ -15,6 +23,6 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
+  # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
   # Return the first valid call found, or return nil if no valid calls are found
 end
